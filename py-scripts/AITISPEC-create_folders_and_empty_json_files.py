bf467efb650d1e40f5e086079f5c989441ef85a6
aitispec <aitispec@ya.ru>
2026-05-02 21:10:37 +0300

    update json creator

--- py-scripts/AITISPEC-create_folders_and_empty_json_files.py
+++ py-scripts/AITISPEC-create_folders_and_empty_json_files.py
@@ -1,16 +1,25 @@
 import os
+import json
 
-# Создаем папки
+# Данные для записи в каждый JSON файл
+data_template = [
+	{
+		"role_1": "empty_pass",
+		"topic": "empty_pass",
+		"sub_topic": "empty_pass",
+		"message_1": "empty_pass"
+	}
+]
+
+# Создаем папки и файлы
 for i in range(1, 26):
 	folder_name = f"{i:03d}"
 	os.makedirs(folder_name, exist_ok=True)
 
-	# В каждой папке создаем 25 пустых json файлов
 	for j in range(1, 26):
 		file_name = f"{folder_name}_{j:03d}.json"
 		file_path = os.path.join(folder_name, file_name)
-		# Создаем пустой json файл с помощью pass
 		with open(file_path, 'w', encoding='utf-8') as f:
-			f.write('{}')
+			json.dump(data_template, f, indent=4, ensure_ascii=False)
 
-print("Папки и пустые json файлы созданы успешно!")
+print("Папки и JSON файлы созданы успешно!")
